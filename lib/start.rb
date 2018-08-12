b1616c888c4d8fa95c64ebaa8596a48e2654b9cd
Yunolan <thorpejoe4@gmail.com>
2018-08-12 08:50:33 +0800

    Updating config

--- lib/start.rb
+++ lib/start.rb
@@ -1,3 +1,6 @@
 require_relative 'LenovoSupport'
-p = LenovoSupport::ProductParser.new "LR04MPAY"
-puts p.data
+product = LenovoSupport::ProductParser.new "LR04MPAY"
+contents = LenovoSupport::ContentParser.new "LR04MPAY"
+
+require_relative 'LenovoSupport'; content_p = LenovoSupport::ContentParser.new "LR04MPAY"
+
